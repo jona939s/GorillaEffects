5911bb6ea5887a7a64adad29aa5c1608864c87d6
JJoe <48448023+jona939s@users.noreply.github.com>
2023-05-17 23:18:19 +0200

    Added basic and 3d particle builder
    Simple way to quickly make particles

--- GorillaVFX/GorillaVFX/Main.cs
+++ GorillaVFX/GorillaVFX/Main.cs
@@ -2,6 +2,7 @@
 using BepInEx.Logging;
 using GorillaVFX.Util;
 using HarmonyLib;
+using UnityEngine;
 
 namespace GorillaVFX
 {
@@ -17,6 +18,11 @@ namespace GorillaVFX
         {
             manualLogSource = Logger; // Allows it to be accessible outside of this type
             $"Init : {GUID}".Log(); // formatted logging
+
+            ParticleSystem ps = ParticleCreator.BasicParticle(false, 2, 1, 10, 10, Color.red);
+            ParticleSystem ps3d = ParticleCreator.Basic3DParticle(PrimitiveType.Sphere, false, 2, 1, 10, 10, Color.black);
+            ps.Play();
+            ps3d.Play();
 
             Bepinject.Zenjector.Install<computerInterface.MainInstaller>().OnProject();
             new Harmony(GUID).PatchAll();
---
+++ GorillaVFX/GorillaVFX/Util/ParticleCreator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GorillaVFX.Util
+{
+    internal class ParticleCreator
+    {
+        private static ParticleSystem ps = new ParticleSystem();
+
+        private static ParticleSystem.MainModule getModule()
+        {
+            return ps.main;
+        }
+
+        internal static ParticleSystem BasicParticle( bool loop, float lifetime, float lifetimeMultiplier, float size, float speed, Color color)
+        {
+            ParticleSystem.MainModule module = getModule();
+
+            module.loop = loop;
+            module.startLifetime = lifetime;
+            module.startLifetimeMultiplier = lifetimeMultiplier;
+            module.startSize = size;
+            module.startSpeed = speed;
+            module.startColor = color;
+
+            return ps;
+        }
+
+        internal static ParticleSystem Basic3DParticle(PrimitiveType type, bool loop, float lifetime, float lifetimeMultiplier, float size, float speed, Color color)
+        { // Define and get mesh
+            ParticleSystem.MainModule module = getModule();
+            GameObject shape = GameObject.CreatePrimitive(type);
+            Mesh mesh = shape.GetComponent<MeshFilter>().sharedMesh;
+            GameObject.Destroy(shape);
+            var psShape = ps.shape;
+
+            // Make PS use the mesh
+            psShape.enabled = true;
+            psShape.shapeType = ParticleSystemShapeType.Mesh;
+            psShape.mesh = mesh;
+
+            // PS settings
+            module.loop = loop;
+            module.startLifetime = lifetime;
+            module.startLifetimeMultiplier = lifetimeMultiplier;
+            module.startSize = size;
+            module.startSpeed = speed;
+            module.startColor = color;
+
+            return ps;
+        }
+    }
+}
