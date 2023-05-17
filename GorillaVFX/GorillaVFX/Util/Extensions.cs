1a92e59d5e2a0e1565ccb7178fa751398950bc7b
Crafterbot <118089496+CrafterBotOfficial@users.noreply.github.com>
2023-05-17 17:14:53 -0400

    Main page
    The formatting may be a little off but I will fix it when I have time to test it

--- GorillaVFX/GorillaVFX/Util/Extensions.cs
+++ GorillaVFX/GorillaVFX/Util/Extensions.cs
@@ -1,4 +1,6 @@
 ﻿using BepInEx.Logging;
+using ComputerInterface;
+using System.Text;
 
 namespace GorillaVFX.Util
 {
@@ -9,6 +11,20 @@ namespace GorillaVFX.Util
 #if DEBUG
             Main.manualLogSource.Log(logLevel, obj);
 #endif 
+        }
+        internal static StringBuilder AddHeader(this StringBuilder stringBuilder, string Title, string Author = "", int Offset = 2)
+        {
+            stringBuilder
+                .BeginCenter()
+                .MakeBar('=', ComputerInterface.ViewLib.ComputerView.SCREEN_WIDTH, 0)
+                .AppendLine(Title);
+            if (Author != "") stringBuilder.AppendLine("<color=ffffff50>" + Author + "</color>");
+            stringBuilder
+                .AppendLine()
+                .MakeBar('=', ComputerInterface.ViewLib.ComputerView.SCREEN_WIDTH, 0)
+                .AppendLines(Offset)
+                .EndAlign();
+            return stringBuilder;
         }
     }
 }
--- GorillaVFX/GorillaVFX/computerInterface/Views/mainView.cs
+++ GorillaVFX/GorillaVFX/computerInterface/Views/mainView.cs
@@ -1,16 +1,62 @@
-﻿using ComputerInterface.Interfaces;
+﻿using ComputerInterface;
+using ComputerInterface.Interfaces;
 using ComputerInterface.ViewLib;
+using GorillaVFX.Util;
 using System;
+using System.Text;
 
 namespace GorillaVFX.computerInterface.Views
 {
     internal class mainView : ComputerView
     {
-        // I will do this when I get back on. Prefable we will have settings to dick tate particle levels and things like that.
+        // --I will do this when I get back on.-- Prefable we will have settings to dick tate particle levels and things like that.
         internal class mainEntry : IComputerModEntry
         {
             public string EntryName => Main.NAME;
             public Type EntryViewType => typeof(Views.mainView);
+        }
+
+        /* Page creation methods */
+
+        private static int _SelectedIndex;
+        private UISelectionHandler selectionHandler;
+        public override void OnShow(object[] args)
+        {
+            base.OnShow(args);
+
+            selectionHandler = new UISelectionHandler(EKeyboardKey.Up, EKeyboardKey.Down);
+            selectionHandler.MaxIdx = 1;
+            selectionHandler.CurrentSelectionIndex = _SelectedIndex;
+            selectionHandler.OnSelected += SelectionHandler_OnSelected;
+            selectionHandler.ConfigureSelectionIndicator("<color=#ed6540>></color>", "", " ", "");
+
+            DrawPage();
+        }
+
+        private void DrawPage()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder
+                .AddHeader(Main.NAME, "By JJoe & Crafterbot", 3)
+                .BeginAlign("right")
+                .AppendLine(selectionHandler.GetIndicatedText(0, "Settings"))
+                .AppendLine(selectionHandler.GetIndicatedText(1, "Credits"));
+        }
+
+        /* Event handling methods */
+        private void SelectionHandler_OnSelected(int obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnKeyPressed(EKeyboardKey key)
+        {
+            if (selectionHandler.HandleKeypress(key))
+            {
+                _SelectedIndex = selectionHandler.CurrentSelectionIndex;
+                DrawPage();
+                return;
+            }
         }
     }
 }
